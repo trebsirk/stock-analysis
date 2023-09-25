cf2b1cb0e37fc683484c6c40543cb25a22157fc9
trebsirk <trebsirk@gmail.com>
2023-09-25 16:46:17 -0400

    refactoring backend/stocks

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
-from stocks import get_stock_data
+from stocks.stocks import get_stock_data
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def index():
     return {"Hello": "World"}
 
 
 @app.get("/stock/{ticker}")
-async def read_item(ticker: str):
-    return get_stock_data(ticker)
+async def get_stock(ticker: str):
+    raw: dict = get_stock_data(ticker)
+    return raw
