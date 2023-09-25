2cee2ee9d93438f196ec51ff0a1a9e169f3e0bd2
trebsirk <trebsirk@gmail.com>
2023-09-25 16:58:47 -0400

    adding caching to endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi_cache.decorator import cache
 from stocks.stocks import get_stock_data
 
 app = FastAPI()
@@ -12,6 +13,7 @@ async def index():
 
 
 @app.get("/stock/{ticker}")
+@cache()
 async def get_stock(ticker: str):
     raw: dict = get_stock_data(ticker)
     return raw
